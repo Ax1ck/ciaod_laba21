5f01f116b9df558a14abc38202bbe44996451ce1
Дмитрий Васильев <dima_vasilev_04@bk.ru>
2024-06-06 23:15:56 +0300

    Добавьте файлы проекта.

---
+++ ciaod_laba12-13/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ciaod_laba12_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewCheckBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            ColumnSorted = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            button2 = new Button();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.BackgroundColor = SystemColors.ActiveCaption;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, ColumnSorted });
+            dataGridView1.GridColor = SystemColors.HotTrack;
+            dataGridView1.Location = new Point(11, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(913, 376);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.Width = 50;
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Название сортировки";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            Column2.Resizable = DataGridViewTriState.True;
+            Column2.SortMode = DataGridViewColumnSortMode.NotSortable;
+            Column2.Width = 160;
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Сравнения";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            Column3.Resizable = DataGridViewTriState.True;
+            Column3.SortMode = DataGridViewColumnSortMode.NotSortable;
+            Column3.Width = 160;
+            // 
+            // Column4
+            // 
+            Column4.HeaderText = "Перестановки";
+            Column4.MinimumWidth = 6;
+            Column4.Name = "Column4";
+            Column4.ReadOnly = true;
+            Column4.Resizable = DataGridViewTriState.True;
+            Column4.SortMode = DataGridViewColumnSortMode.NotSortable;
+            Column4.Width = 160;
+            // 
+            // Column5
+            // 
+            Column5.HeaderText = "Время";
+            Column5.MinimumWidth = 6;
+            Column5.Name = "Column5";
+            Column5.ReadOnly = true;
+            Column5.Width = 170;
+            // 
+            // ColumnSorted
+            // 
+            ColumnSorted.HeaderText = "Отсортировано?";
+            ColumnSorted.MinimumWidth = 6;
+            ColumnSorted.Name = "ColumnSorted";
+            ColumnSorted.Width = 160;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(817, 393);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 59);
+            button1.TabIndex = 1;
+            button1.Text = "выход";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(429, 411);
+            numericUpDown1.Margin = new Padding(3, 4, 3, 4);
+            numericUpDown1.Maximum = new decimal(new int[] { 100000000, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(129, 27);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.Value = new decimal(new int[] { 10000, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(301, 413);
+            label1.Name = "label1";
+            label1.Size = new Size(122, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Размер массива";
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Location = new Point(14, 399);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(245, 53);
+            button2.TabIndex = 4;
+            button2.Text = "Сортировать";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(605, 549);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 20);
+            label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(932, 455);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Васильев лабораторная 12-13";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Button button2;
+        private DataGridViewCheckBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn ColumnSorted;
+        private Label label2;
+    }
+}
---
+++ ciaod_laba12-13/Form1.cs
@@ -0,0 +1,500 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using static ciaod_laba12_13.Form1;
+
+namespace ciaod_laba12_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(8);
+            dataGridView1.Rows[0].Cells[1].Value = "Обмен";
+            dataGridView1.Rows[1].Cells[1].Value = "Выбор";
+            dataGridView1.Rows[2].Cells[1].Value = "Включение";
+            dataGridView1.Rows[3].Cells[1].Value = "Быстрая";
+            dataGridView1.Rows[4].Cells[1].Value = "Шелла";
+            dataGridView1.Rows[5].Cells[1].Value = "Линейная";
+            dataGridView1.Rows[6].Cells[1].Value = "Встроенная";
+            dataGridView1.Rows[7].Cells[1].Value = "Пирамидальная";
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = true;
+            }
+
+        }
+        public struct result
+        {
+            public ulong time;
+            public ulong comp;
+            public ulong reinst;
+            public int[] newArray;
+            public result()
+            {
+                time =comp=reinst= 0;
+            }
+            public result(long _time, int _comp,int _reinst, int[] _newArray)
+            {
+                time= (ulong)_time;
+                comp= (ulong)_comp;
+                reinst= (ulong)_reinst;
+                newArray = _newArray;
+            }
+            public result(long _time, ulong _comp, ulong _reinst, int[] _newArray)
+            {
+                time = (ulong)_time;
+                comp = _comp;
+                reinst = _reinst;
+                newArray = _newArray;
+            }
+        }
+        public bool Check(int[] Array)
+        {
+            for (int i = 1;i< Array.Length;i++)
+            {
+                if (Array[i] < Array[i-1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        public static result BubbleSort(int[] Array)
+        {
+            Stopwatch time = new Stopwatch();
+            int comp  = 0;
+            int reinst = 0;
+            time.Start();
+            bool have_reinst=true;
+            for (int i = 0; i < Array.Length - 1; i++)
+            {
+                have_reinst = false;
+                for (int j = 0; j < Array.Length-i - 1; j++)
+                {
+                    comp++;
+                    if (Array[j] > Array[j + 1])
+                    {
+                        have_reinst = true;
+                        reinst++;
+                        int temp = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = temp;
+                    }
+                }
+                if (have_reinst == false)
+                {
+                    break;
+                }
+                if (have_reinst == false)
+                {
+                    break;
+                }
+            }
+            time.Stop();
+            return new result(time.ElapsedMilliseconds,comp, reinst,Array);
+        }
+
+        static public result ViborSort(int[] Array)
+        {
+            Stopwatch time = new Stopwatch();
+            int comp = 0;
+            int reinst = 0;
+            time.Start();
+            for (int i = 0; i < Array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < Array.Length; j++)
+                {
+                    comp++;
+                    if (Array[j] < Array[min])
+                    {
+                        min = j;
+                    }
+                }
+                reinst++;
+                int temp = Array[min];
+                Array[min] = Array[i];
+                Array[i] = temp;
+            }
+            time.Stop();
+            return new result(time.ElapsedMilliseconds, comp, reinst, Array);
+        }
+
+        static public result SortingByDirectInclusions(int[] Array)
+        {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            int comp = 0;
+            int reinst = 0;
+            int min = Array[0];
+            int j = 0;
+            for (int i = 1; i < Array.Length; i++)
+            {
+                comp++;
+                if (Array[i] < min)
+                {
+                    min = Array[i];
+                    j = i;
+                }
+            }
+            Array[j] = Array[0];
+            Array[0] = min;
+            reinst += 2;
+            for (int i = 1; i < Array.Length; i++)
+            {
+                int value = Array[i]; 
+                int index = i;     
+                while ((index > 0) && (Array[index - 1] > value))
+                {
+                    comp++;
+                    reinst++;
+                    Array[index] = Array[index - 1];
+                    index--;    
+                }
+                comp++;
+                reinst++;
+                Array[index] = value;
+            }
+            time.Stop();
+            return new result(time.ElapsedMilliseconds,comp, reinst, Array);
+        }
+
+        static public result Quicksort(int[] array,int startindex,int endindex)
+        {
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            int index = startindex;
+            ulong comp = 0;
+            ulong reinst = 0;
+            for (int i = startindex+1; i < endindex; i++)
+            {
+                comp++;
+                if (array[i] < array[index])
+                {
+                    reinst += 2;
+                    int tmp = array[index];
+                    array[index] = array[i];
+                    array[i]= array[index+1];
+                    array[index+1] = tmp;
+                    index++;
+                }
+            }
+
+            result res1 = QuicksortWT(array,startindex,index);
+            result res2 = QuicksortWT(array,index+1,endindex);
+            time.Stop();
+            return new result(time.ElapsedMilliseconds, comp + res1.comp+res2.comp, reinst+res1.reinst+res2.reinst, array);
+        }
+        static public result QuicksortWT(int[] array, int startindex, int endindex)
+        {
+            if (startindex >= endindex)
+            {
+                result result = new result();
+                result.comp = 1;
+                return result;
+            }
+            int index = startindex;
+            ulong comp = 0;
+            ulong reinst = 0;
+            for (int i = startindex + 1; i < endindex; i++)
+            {
+                comp++;
+                if (array[i] < array[index])
+                {
+                    reinst += 2;
+                    int tmp = array[index];
+                    array[index] = array[i];
+                    array[i] = array[index + 1];
+                    array[index + 1] = tmp;
+                    index++;
+                }
+            }
+
+            result res1 = Quicksort(array, startindex, index);
+            result res2 = Quicksort(array, index + 1, endindex);
+            result res = new result();
+            res.comp = comp+res1.comp+res2.comp;
+            res.reinst=reinst+res1.reinst+res2.reinst;
+            return res;
+        }
+
+        result ShellSort(int[] array)
+        {
+            ulong comp = 0;
+            ulong reinst = 0;
+            var sw = new Stopwatch();
+            sw.Start();
+            int j;
+            int step = 1;
+            while (step <= array.Length / 9)
+            {
+                step = 3 * step + 1;
+            }
+            while (step > 0)
+            {
+                for (int i = 0; i < (array.Length - step); i++)
+                {
+                    j = i;
+                    int tmp = array[j + step];
+                    while ((j >= 0) && (array[j] > tmp))
+                    {
+                        comp++;
+                        array[j + step] = array[j];
+                        j -= step;
+                        reinst++;
+                    }
+                    array[j + step] = tmp;
+
+                    comp++;
+                }
+                step = (step - 1) / 3;
+            }
+
+            sw.Stop();
+            return new result(sw.ElapsedMilliseconds, comp, reinst, array);
+        }
+        result LinearSort(int[] array)
+        {
+            ulong comp = 0;
+            ulong reinst = 0;
+            int[] tmpArray = new int[array.Length];
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < array.Length; i++)
+            {
+                tmpArray[i] = 0;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                comp++;
+                reinst++;
+                tmpArray[array[i]]++;
+            }
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < tmpArray[i]; j++)
+                {
+                    reinst++;
+                    array[index] = i;
+                    index++;
+                }
+            }
+            sw.Stop();
+            return new result(sw.ElapsedMilliseconds, comp, reinst, array);
+        }
+
+
+        void fixDown(int[] Array, int n, int i, ref int comp, ref int swaps)
+        {
+            while (2 * i + 1 < n)
+            {
+                int j = 2 * i + 1;
+                comp++;
+                if (j + 1 < n && Array[j] < Array[j + 1]) j++;
+                comp++;
+                if (Array[i] >= Array[j]) break;
+                swaps++;
+                (Array[i], Array[j]) = (Array[j], Array[i]);
+                i = j;
+            }
+        }
+
+        result Pyrsort(int[] array)
+        {
+            int comp = 0;
+            int swaps = 0;
+            var sw = new Stopwatch();
+
+            sw.Start();
+            for (int i = array.Length / 2 - 1; i >= 0; i--)
+                fixDown(array, array.Length, i, ref comp, ref swaps);
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                swaps++;
+                (array[0], array[i]) = (array[i], array[0]);
+                fixDown(array, i, 0, ref comp, ref swaps);
+            }
+            sw.Stop();
+            return new result(sw.ElapsedMilliseconds, comp, swaps, array);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label2.Text= string.Empty;
+            for(int i=0;i< dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 2; j < 6; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = "";
+                }
+            }
+            Random rnd = new Random();
+            int[] array = new int[Convert.ToInt32( numericUpDown1.Value)];
+            for(int i=0; i<array.Length; i++)
+            {
+                array[i] = rnd.Next(0, array.Length);
+            }
+            result res;
+            if (dataGridView1.Rows[0].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                res = BubbleSort(array_tmp);
+                dataGridView1.Rows[0].Cells[2].Value = Convert.ToString(res.comp);
+                dataGridView1.Rows[0].Cells[3].Value = Convert.ToString(res.reinst);
+                dataGridView1.Rows[0].Cells[4].Value = Convert.ToString(res.time);
+                if (Check(res.newArray))
+                {
+                    dataGridView1.Rows[0].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[0].Cells[5].Value = "нет";
+                }
+            }
+            if (dataGridView1.Rows[1].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                res = ViborSort(array_tmp);
+                dataGridView1.Rows[1].Cells[2].Value = Convert.ToString(res.comp);
+                dataGridView1.Rows[1].Cells[3].Value = Convert.ToString(res.reinst);
+                dataGridView1.Rows[1].Cells[4].Value = Convert.ToString(res.time);
+                if (Check(res.newArray))
+                {
+                    dataGridView1.Rows[1].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[1].Cells[5].Value = "нет";
+                }
+            }
+            if (dataGridView1.Rows[2].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                res =SortingByDirectInclusions(array_tmp);
+                dataGridView1.Rows[2].Cells[2].Value = Convert.ToString(res.comp);
+                dataGridView1.Rows[2].Cells[3].Value = Convert.ToString(res.reinst);
+                dataGridView1.Rows[2].Cells[4].Value = Convert.ToString(res.time);
+                if (Check(res.newArray))
+                {
+                    dataGridView1.Rows[2].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[2].Cells[5].Value = "нет";
+                }
+            }
+            if (dataGridView1.Rows[3].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                 res = Quicksort(array_tmp,0,array_tmp.Length);
+                dataGridView1.Rows[3].Cells[2].Value = Convert.ToString(res.comp);
+                dataGridView1.Rows[3].Cells[3].Value = Convert.ToString(res.reinst);
+                dataGridView1.Rows[3].Cells[4].Value = Convert.ToString(res.time);
+                if (Check(res.newArray))
+                {
+                    dataGridView1.Rows[3].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[3].Cells[5].Value = "нет";
+                }
+            }
+            if (dataGridView1.Rows[4].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                res = ShellSort(array_tmp);
+                dataGridView1.Rows[4].Cells[2].Value = Convert.ToString(res.comp);
+                dataGridView1.Rows[4].Cells[3].Value = Convert.ToString(res.reinst);
+                dataGridView1.Rows[4].Cells[4].Value = Convert.ToString(res.time);
+                if (Check(res.newArray))
+                {
+                    dataGridView1.Rows[4].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[4].Cells[5].Value = "нет";
+                }
+            }
+            if (dataGridView1.Rows[5].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                res = LinearSort(array_tmp);
+                dataGridView1.Rows[5].Cells[2].Value = Convert.ToString(res.comp);
+                dataGridView1.Rows[5].Cells[3].Value = Convert.ToString(res.reinst);
+                dataGridView1.Rows[5].Cells[4].Value = Convert.ToString(res.time);
+                if (Check(res.newArray))
+                {
+                    dataGridView1.Rows[5].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[5].Cells[5].Value = "нет";
+                }
+            }
+            if (dataGridView1.Rows[6].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                var sw = new Stopwatch();
+                sw.Start();
+                Array.Sort(array_tmp);
+                sw.Stop();
+                dataGridView1.Rows[6].Cells[2].Value = "-";
+                dataGridView1.Rows[6].Cells[3].Value = "-";
+                dataGridView1.Rows[6].Cells[4].Value = Convert.ToString(sw.ElapsedMilliseconds);
+                if (Check(array_tmp))
+                {
+                    dataGridView1.Rows[6].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[6].Cells[5].Value = "нет";
+                }
+            }
+            if (dataGridView1.Rows[7].Cells[0].Value.Equals(true))
+            {
+                int[] array_tmp = (int[])array.Clone();
+                res = Pyrsort(array_tmp);
+                dataGridView1.Rows[7].Cells[2].Value = Convert.ToString(res.comp);
+                dataGridView1.Rows[7].Cells[3].Value = Convert.ToString(res.reinst);
+                dataGridView1.Rows[7].Cells[4].Value = Convert.ToString(res.time);
+                if (Check(array_tmp))
+                {
+                    dataGridView1.Rows[7].Cells[5].Value = "да";
+                }
+                else
+                {
+                    dataGridView1.Rows[7].Cells[5].Value = "нет";
+                }
+            }
+            bool cheked = false;
+            for(int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value.Equals(true))
+                {
+                    cheked = true;
+                    break;
+                }
+
+            }
+            if (!cheked) {
+                label2.Visible = true;
+                label2.Text = "Ничего не выбрано";
+            }
+
+        }
+    }
+}
